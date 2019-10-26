7d2b6aecdc4f9ffd4b2904724af2eadb796d4fb2
Tony Messias <tonyzrp@gmail.com>
2019-10-25 23:13:35 -0300

    Tweak the navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,20 @@
 <template>
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    <nav class="flex items-center justify-between flex-wrap bg-orange-500 p-6">
+        <div class="flex items-center flex-shrink-0 text-orange-100 mr-6">
+            <img src="../assets/logo.png" class="h-8 border-2 border-orange-300 rounded" alt="">
+            <span class="ml-2 font-semibold text-xl tracking-tight">HarvestApp for Linux</span>
         </div>
         <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <button @click.prevent="showMenu = !showMenu" class="flex items-center px-3 py-2 border border-orange-200 rounded text-teal-200 hover:text-orange-100 hover:border-orange-100">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Docs
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Examples
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                    Blog
-                </a>
-            </div>
+        <div class="w-full block flex-grow lg:flex justify-end lg:w-auto" :class="{'sm:hidden': !showMenu}">
             <div>
-                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+                <a href="#responsive-header" class="block mr-4 mt-4 lg:inline-block lg:mt-0 text-orange-200 hover:text-orange-100">
+                    Tony Messias
+                </a>
+                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-orange-200 border-orange-200 hover:border-transparent hover:text-orange-500 hover:bg-orange-100 mt-4 lg:mt-0">Login</a>
             </div>
         </div>
     </nav>
@@ -30,6 +22,11 @@
 
 <script>
 export default {
-    name: "Navigation"
-}
+    name: "Navigation",
+    data() {
+        return {
+            showMenu: false,
+        };
+    },
+};
 </script>
